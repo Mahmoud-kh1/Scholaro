b7be7776d6295b4217a130b9000d30f3e693e15c
Mahmoud-kh1 <hhh116716@gmail.com>
2025-09-27 09:13:42 +0300

    intial commit

---
+++ src/main/java/com/learnharbor/Scholaro/ScholaroApplication.java
@@ -0,0 +1,13 @@
+package com.learnharbor.Scholaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnharbor/Scholaro/ScholaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnharbor.Scholaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
